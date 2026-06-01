2bd8ee61c3cf7a2d2865d07d76a882c407f5fe05
David Gili <d.gili.lopez@gmail.com>
2026-06-01 18:35:04 -0400

    morgan importado y habilitado

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express"); //Importamos el paquete express
+const morgan = require("morgan"); //Importamos librería morgan
 const cors = require("cors"); //Importamos el paquete cors
 require("dotenv").config(); //Cargamos las variables del .env
 const app = express(); //Asignamos la instancia de express a la constante app
@@ -8,6 +9,7 @@ const { obtenerJoyas, obtenerJoyasConFiltros } = require("./consultas"); //Impor
 //middlewares
 app.use(cors()); //Habilitamos cors
 app.use(express.json()); //Habilitamos el parseo de datos JSON
+app.use(morgan("dev")); //Habilitamos el middleware morgan
 
 //Creamos el puerto de escucha
 app.listen(puerto, console.log(`Servidor encendido escuchando puerto ${puerto}`));
